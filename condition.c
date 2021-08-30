d2f2ed6112ac2cba155df4db913d1c5ac7c8550d
InsaneObsession69 <89762088+InsaneObsession69@users.noreply.github.com>
2021-08-30 16:31:32 +0530

    Create condition.c

---
+++ condition.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    
+    int x = get_int("x:");
+    
+    int y = get_int("y:");
+    
+    
+    if ("x > y")
+    
+    {printf("x is greater than y");}
+    
+    else if ("x < y")
+    
+    {printf("x is smaller than  y");}
+    
+    else {printf("x is equal to y");}
+    
+
+}
